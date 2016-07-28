56ab55e6ab22193f011ca5c90627b002617609b8
Saiaswin Saikannan <saiaswin90@gmail.com>
2016-07-28 15:14:36 +0800

    Add integer with value 4

--- Test.java
+++ Test.java
@@ -1,4 +1,7 @@
 public class Test{
+
+  private static final Integer FOUR_INTEGER = 4;
+
   public static void main(String[] args){
       System.out.println("Hello World!");
       System.out.println("World is great!");
